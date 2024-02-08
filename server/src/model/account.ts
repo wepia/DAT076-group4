2507138b6d8da52d3f15fa63362d7e3d3d74cdfb
wepia1 <wepia@student.chalmers.se>
2024-02-08 15:39:22 +0100

    account-model updated

--- server/src/model/account.ts
+++ server/src/model/account.ts
@@ -1,7 +1,20 @@
 export interface Account{
+    id : number;
     userName : string;
     email : string;
     password : string;
     gender : string;
     birth : string;
+    shifts: Shift[];
+    /* Potential useful attributes to add later:
+        availableTime[]
+        events[] (events the user is organizing)
+     */
 }
+
+export interface Shift {
+    eventID: number;
+    shiftID: number;
+}
+
+//possibly add an extended "organizer account"?
