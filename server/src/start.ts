a35b44c3f44cd42f316959c64fea68f0677b9575
Algeskog <algeskog@student.chalmers.se>
2024-02-13 19:05:20 +0100

    Setup for lab assignment 3

--- server/src/start.ts
+++ server/src/start.ts
@@ -2,10 +2,8 @@ import express from "express";
 import { accountRouter } from "./router/account";
 import { eventApi } from "./router/eventApi";
 
-const cors = require('cors'); 
 export const app = express();
 
-app.use(cors());
 app.use(express.json());
 app.use("/account", accountRouter);
 app.use("/event", eventApi);
