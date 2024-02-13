9d6796725c266a56a1aa4dc8c1980587e454c65c
Algeskog <algeskog@student.chalmers.se>
2024-02-13 19:11:29 +0100

    Added cors according to instructions in start.ts

--- server/src/start.ts
+++ server/src/start.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import { accountRouter } from "./router/account";
 import { eventApi } from "./router/eventApi";
 
 export const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/account", accountRouter);
 app.use("/event", eventApi);
