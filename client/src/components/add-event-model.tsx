e3b5865e8404bd060c2b6bb41e6e699e86282aef
wepia1 <wepia@student.chalmers.se>
2024-02-21 21:11:58 +0100

    create new event

--- client/src/components/add-event-model.tsx
+++ client/src/components/add-event-model.tsx
@@ -1,6 +1,37 @@
-import { Modal, Button } from "react-bootstrap";
+import axios from "axios";
+import { FormEvent, useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+
+interface Event {
+}
 
 function AddEventModel(props : {show: boolean, handleClose: any}) {
+  const [newName, setNewName] = useState<string>("");
+  const [newOrganizer, setNewOrganizer] = useState<string>("");
+  const [newDate, setNewDate] = useState<string>("");
+
+  async function submitEvent (e : FormEvent) {
+    try {
+      e.preventDefault();
+      await axios.post('http://localhost:8080/event',
+        { 
+          name : newName,
+          organizer : newOrganizer,
+          date : newDate
+         }
+      );
+      //TODO update event list
+      props.handleClose();
+    } catch (err: any) {
+      if (err.response) {
+        console.log(err.response.status);
+      } else if (err.request) {
+        console.log("No response from server")
+      } else {
+        console.log(err);
+      }
+    }
+  };
 
   return (
     <>
@@ -9,25 +40,28 @@ function AddEventModel(props : {show: boolean, handleClose: any}) {
           <Modal.Title>Add New Event</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <form action="">
+          <Form onSubmit={e => submitEvent(e)}>
             <div className="mb-3">
               <label htmlFor="eventName" className="form-label">Event Name</label>
-              <input type="text" id="eventName" className="form-control" required />
+              <input type="text" id="eventName" className="form-control" required 
+                onChange={(e => {setNewName(e.target.value);})} />
             </div>
             <div className="mb-3">
               <label htmlFor="eventOrganizer" className="form-label">Organizer</label>
-              <input type="text" id="eventOrganizer" className="form-control" required />
+              <input type="text" id="eventOrganizer" className="form-control" required 
+                onChange={(e => {setNewOrganizer(e.target.value);})}/>
             </div>
             <div className="mb-3">
               <label htmlFor="eventDate" className="form-label">Date</label>
-              <input type="date" id="eventDate" className="form-control" required />
+              <input type="date" id="eventDate" className="form-control" required 
+                onChange={(e => {setNewDate(e.target.value);})}/>
             </div>
-          </form>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={props.handleClose}>Cancel</Button>
+              <Button variant="primary" type="submit">Ok</Button>
+            </Modal.Footer>
+          </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={props.handleClose}>Cancel</Button>
-          <Button variant="primary" onClick={props.handleClose}>Ok</Button>
-        </Modal.Footer>
       </Modal>
     </>
   );
