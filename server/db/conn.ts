390f73106c0eb590f022ef255e8a15122196afec
wepia1 <wepia@student.chalmers.se>
2024-02-29 12:59:01 +0100

    application access uri (not compass)

--- server/db/conn.ts
+++ server/db/conn.ts
@@ -3,7 +3,7 @@ import { Connection, createConnection } from "mongoose";
 
 
 export async function makeConnection() : Promise<Connection> {
-   return createConnection("mongodb+srv://andze:Zetterqvist5110@cluster0.ocm2oft.mongodb.net/");   
+   return createConnection("mongodb+srv://andze:Zetterqvist5110@cluster0.ocm2oft.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");   
 } 
 
 export const conn : Promise<Connection> = makeConnection();
