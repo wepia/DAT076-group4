30740266e7a6e83f31d5d9cd7d2f9a2b351fcf88
wepia1 <wepia@student.chalmers.se>
2024-03-07 14:29:08 +0100

    old accountservice moved

--- old_server/account.ts
+++ old_server/account.ts
@@ -1,7 +1,26 @@
 import {Account} from "../server/src/model/account";
+import { SportEvent } from "../server/src/model/sportEvent";
 import { IAccountService } from "../server/src/service/account.interface";
 
 export class AccountService implements IAccountService{
+    findAccount(username: string, password: string): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    getAccountEvents(userName: string): Promise<string[]> {
+        throw new Error("Method not implemented.");
+    }
+    addEvent(userName: string, eventID: string): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    removeEvent(userName: string, eventID: string): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    changeEmail(userName: string, password: string, newEmail: string): Promise<boolean> {
+        throw new Error("Method not implemented.");
+    }
+    accessAccount(userName: string, password: string): Promise<Account> {
+        throw new Error("Method not implemented.");
+    }
     private accounts : Account[] = [];
 
 
@@ -13,11 +32,11 @@ export class AccountService implements IAccountService{
 
         let newAcc : Account = {
             userName: userName,
-            password: password,
-            email: email,
-            gender: gender,
-            birth: birth,
-            events: []
+            password : password,
+            email : email,
+            gender : gender,
+            birth : birth,
+            eventIDs : []
         }
 
         this.accounts.push(newAcc);
